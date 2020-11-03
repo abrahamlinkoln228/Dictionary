57aa3443ca707c8cbf0259eb31095d643cfe7939
Your Name <you@example.com>
2020-11-03 15:03:04 +0200

    list of all possible words

---
+++ WordPrem.java
@@ -0,0 +1,25 @@
+package com.javatpoint;  
+public class WordPrem{
+  public static void main(String[] args) {
+	char[] chars = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+	int len = 5;
+	iterate(chars, len, new char[len], 0);
+
+  }
+
+  public static void iterate(char[] chars, int len, char[] build, int pos) {
+	if (pos == len) {
+		String word = new String(build);
+		System.out.println(word);
+	return;
+        }
+
+	for (int i = 0; i < chars.length; i++) {
+		build[pos] = chars[i];
+		iterate(chars, len , build, pos + 1);
+	}
+
+  }
+
+
+}
